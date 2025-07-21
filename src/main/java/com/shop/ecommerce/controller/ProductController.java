41975719c58cf3c4d94eb022f4004fa9ae9e6305
alimohtarami <alimohtarami5@gmail.com>
2025-07-21 20:33:05 +0330

    ProductService,ProductController

--- src/main/java/com/shop/ecommerce/controller/ProductController.java
+++ src/main/java/com/shop/ecommerce/controller/ProductController.java
@@ -6,11 +6,35 @@ import com.shop.ecommerce.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
     private final ProductService productService;
     public ProductController(ProductService productService){
         this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest request){
+        return ResponseEntity.ok(productService.createProduct(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts(){
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable Long id){
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 }
