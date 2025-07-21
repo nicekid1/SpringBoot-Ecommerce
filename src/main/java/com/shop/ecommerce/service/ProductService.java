2d930d7591a11e5e6f4b7ac0d1ff652e7d5c05cd
alimohtarami <alimohtarami5@gmail.com>
2025-07-21 19:24:46 +0330

    CategoryRepository , ProductService

---
+++ src/main/java/com/shop/ecommerce/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.shop.ecommerce.service;
+
+import com.shop.ecommerce.dto.ProductRequest;
+import com.shop.ecommerce.dto.ProductResponse;
+import com.shop.ecommerce.model.Category;
+import com.shop.ecommerce.model.Product;
+import com.shop.ecommerce.repository.ProductRepository;
+
+
+public class ProductService {
+}
