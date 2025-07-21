6750e9c0370ba9f19610127c226010e640117be3
alimohtarami <alimohtarami5@gmail.com>
2025-07-21 20:14:06 +0330

    ProductService

--- src/main/java/com/shop/ecommerce/service/ProductService.java
+++ src/main/java/com/shop/ecommerce/service/ProductService.java
@@ -4,13 +4,12 @@ import com.shop.ecommerce.dto.ProductRequest;
 import com.shop.ecommerce.dto.ProductResponse;
 import com.shop.ecommerce.model.Category;
 import com.shop.ecommerce.model.Product;
-import com.shop.ecommerce.repository.ProductRepository;
 import com.shop.ecommerce.repository.CategoryRepository;
+import com.shop.ecommerce.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collector;
-
+import java.util.stream.Collectors;
 
 public class ProductService {
 
@@ -20,6 +19,49 @@ public class ProductService {
     public ProductService(ProductRepository productRepository,CategoryRepository categoryRepository){
         this.productRepository = productRepository;
         this.categoryRepository = categoryRepository;
+    }
+
+    public ProductResponse createProduct(ProductRequest request) {
+        Category category = categoryRepository.findById(request.getCategoryId())
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        Product product = Product.builder()
+                .name(request.getName())
+                .description(request.getDescription())
+                .price(request.getPrice())
+                .quantity(request.getQuantity())
+                .category(category)
+                .build();
+
+        Product saved = productRepository.save(product);
+
+        return mapToResponse(saved);
+    }
+
+    public List<ProductResponse> getAllProducts(){
+        return productRepository.findAll().stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    public ProductResponse getProductById(Long id){
+        Product product = productRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Product not found"));
+        return mapToResponse(product);
+    }
+
+    public void deleteProduct(Long id){
+        productRepository.deleteById(id);
+    }
+    private ProductResponse mapToResponse(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getQuantity(),
+                product.getCategory().getName()
+        );
     }
 
 }
