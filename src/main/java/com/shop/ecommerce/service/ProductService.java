ae1bf2dba919ee9c49e302e0772060bdacb6bda7
alimohtarami <alimohtarami5@gmail.com>
2025-07-21 19:48:29 +0330

    ProductService

--- src/main/java/com/shop/ecommerce/service/ProductService.java
+++ src/main/java/com/shop/ecommerce/service/ProductService.java
@@ -5,7 +5,21 @@ import com.shop.ecommerce.dto.ProductResponse;
 import com.shop.ecommerce.model.Category;
 import com.shop.ecommerce.model.Product;
 import com.shop.ecommerce.repository.ProductRepository;
+import com.shop.ecommerce.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collector;
 
 
 public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    public ProductService(ProductRepository productRepository,CategoryRepository categoryRepository){
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
 }
