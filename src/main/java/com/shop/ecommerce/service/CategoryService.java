b5601e5f4b0fb9a72b954e730821050f8a028a1d
alimohtarami <alimohtarami5@gmail.com>
2025-07-22 19:33:30 +0330

    category service

--- src/main/java/com/shop/ecommerce/service/CategoryService.java
+++ src/main/java/com/shop/ecommerce/service/CategoryService.java
@@ -1,4 +1,59 @@
 package com.shop.ecommerce.service;
 
+import com.shop.ecommerce.dto.CategoryRequest;
+import com.shop.ecommerce.dto.CategoryResponse;
+import com.shop.ecommerce.model.Category;
+import com.shop.ecommerce.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
 public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    public CategoryService(CategoryRepository categoryRepository){
+        this.categoryRepository = categoryRepository;
+    }
+
+    @PostMapping
+    public CategoryResponse createCategory(CategoryRequest request){
+        Category category = Category.builder().
+                name(request.getName())
+                .description(request.getDescription())
+                .build();
+        return mapToResponse(categoryRepository.save(category));
+    }
+
+    @GetMapping
+    public List<CategoryResponse> getAllCategories(){
+        return categoryRepository.findAll()
+                .stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public CategoryResponse getAllCategories(Long id){
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("category is not found"));
+        return mapToResponse(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(Long id){
+        categoryRepository.deleteById(id);
+    }
+
+    private CategoryResponse mapToResponse(Category category){
+        return new CategoryResponse(
+                category.getId(),
+                category.getName(),
+                category.getDescription()
+        );
+    }
 }
