f0f37a4e3dce8257c3bb6e9402afa603968c1e4b
alimohtarami <alimohtarami5@gmail.com>
2025-07-22 19:11:42 +0330

    category dto

--- src/main/java/com/shop/ecommerce/dto/CategoryResponse.java
+++ src/main/java/com/shop/ecommerce/dto/CategoryResponse.java
@@ -1,4 +1,14 @@
 package com.shop.ecommerce.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryResponse {
+    private Long id;
+    private String name;
+    private String description;
 }
