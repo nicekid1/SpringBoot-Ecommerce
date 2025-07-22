3e9d1c45f63e5f45969924593cc33652f85b2251
alimohtarami <alimohtarami5@gmail.com>
2025-07-22 20:29:06 +0330

    user dto

--- src/main/java/com/shop/ecommerce/dto/AuthResponse.java
+++ src/main/java/com/shop/ecommerce/dto/AuthResponse.java
@@ -1,4 +1,8 @@
 package com.shop.ecommerce.dto;
 
+import lombok.Data;
+
+@Data
 public class AuthResponse {
+    private String token;
 }
--- src/main/java/com/shop/ecommerce/dto/RegisterRequest.java
+++ src/main/java/com/shop/ecommerce/dto/RegisterRequest.java
@@ -1,4 +1,10 @@
 package com.shop.ecommerce.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
 }
