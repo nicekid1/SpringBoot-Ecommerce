d60a936c7d38f8745ddbe0345f5dd9340f87ae52
alimohtarami <alimohtarami5@gmail.com>
2025-07-23 18:22:32 +0330

    Auth controller

--- src/main/java/com/shop/ecommerce/controller/AuthController.java
+++ src/main/java/com/shop/ecommerce/controller/AuthController.java
@@ -1,4 +1,30 @@
 package com.shop.ecommerce.controller;
 
+import com.shop.ecommerce.dto.AuthRequest;
+import com.shop.ecommerce.dto.AuthResponse;
+import com.shop.ecommerce.dto.RegisterRequest;
+import com.shop.ecommerce.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
 }
