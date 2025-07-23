1743f33cdf3737f85657d78a51d9f42b3ccca61b
alimohtarami <alimohtarami5@gmail.com>
2025-07-23 17:47:21 +0330

    jwt uitls

--- src/main/java/com/shop/ecommerce/security/JwtUtil.java
+++ src/main/java/com/shop/ecommerce/security/JwtUtil.java
@@ -1,8 +1,37 @@
 package com.shop.ecommerce.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
+@Component
 public class JwtUtil {
     @Value("${jwt.secret}")
     private String secret;
+
+    public String generateToken(String email){
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 *10))
+                .signWith(SignatureAlgorithm.ES256,secret)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        return extractUsername(token).equals(userDetails.getUsername());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+}
 }
