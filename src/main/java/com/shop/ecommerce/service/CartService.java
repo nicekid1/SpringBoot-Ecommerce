2b9eb0389c4cd77330d538daa947ce06b83c69a3
alimohtarami <alimohtarami5@gmail.com>
2025-07-31 19:36:50 +0330

    add cart

--- src/main/java/com/shop/ecommerce/service/CartService.java
+++ src/main/java/com/shop/ecommerce/service/CartService.java
@@ -1,4 +1,40 @@
 package com.shop.ecommerce.service;
 
+import com.shop.ecommerce.dto.AddToCartRequest;
+import com.shop.ecommerce.model.Cart;
+import com.shop.ecommerce.model.CartItem;
+import com.shop.ecommerce.model.Product;
+import com.shop.ecommerce.model.User;
+import com.shop.ecommerce.repository.CartItemRepository;
+import com.shop.ecommerce.repository.CartRepository;
+import com.shop.ecommerce.repository.ProductRepository;
+import com.shop.ecommerce.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CartService {
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+    private  final ProductRepository productRepository;
+    private final UserRepository userRepository;
+    public void add(AddToCartRequest request, String email){
+        User user = userRepository.findByEmail(email).orElseThrow();
+        Product product = productRepository.findById(request.getProductId()).orElseThrow();
+
+        Cart cart = cartRepository.findByUser(user)
+                .orElseGet(() -> {
+                    Cart newCart = new Cart();
+                    newCart.setUser(user);
+                    return cartRepository.save(newCart);
+                });
+        CartItem item = CartItem.builder()
+                .cart(cart)
+                .product(product)
+                .quantity(request.getQuantity())
+                .build();
+
+        cartItemRepository.save(item);
+    }
 }
