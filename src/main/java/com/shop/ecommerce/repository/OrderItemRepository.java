66e24113cd024d7c0a9a35a33e95bd0c43e9aeb2
alimohtarami <alimohtarami5@gmail.com>
2025-07-31 21:43:31 +0330

    OrderItemRepository fix bug

--- src/main/java/com/shop/ecommerce/repository/OrderItemRepository.java
+++ src/main/java/com/shop/ecommerce/repository/OrderItemRepository.java
@@ -1,7 +1,7 @@
 package com.shop.ecommerce.repository;
 
-import com.shop.ecommerce.model.Order;
+import com.shop.ecommerce.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<Order,Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
 }
