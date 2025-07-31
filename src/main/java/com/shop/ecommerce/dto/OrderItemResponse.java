28577e1a328bae380c39ae41a54e00a5dad1ac87
alimohtarami <alimohtarami5@gmail.com>
2025-07-31 21:27:42 +0330

    order dto

--- src/main/java/com/shop/ecommerce/dto/OrderItemResponse.java
+++ src/main/java/com/shop/ecommerce/dto/OrderItemResponse.java
@@ -1,4 +1,12 @@
 package com.shop.ecommerce.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class OrderItemResponse {
+    private String productName;
+    private Integer quantity;
+    private Double price;
 }
--- src/main/java/com/shop/ecommerce/dto/OrderResponse.java
+++ src/main/java/com/shop/ecommerce/dto/OrderResponse.java
@@ -1,4 +1,16 @@
 package com.shop.ecommerce.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
 public class OrderResponse {
+    private Long orderId;
+    private LocalDateTime orderDate;
+    private Double totalPrice;
+    private List<OrderItemResponse> items;
 }
