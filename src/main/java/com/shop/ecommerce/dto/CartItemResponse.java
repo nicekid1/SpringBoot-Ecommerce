7c1c3253001f727bfc8cc8cc861e2e6b637334fa
alimohtarami <alimohtarami5@gmail.com>
2025-07-31 19:46:35 +0330

    fix dto

--- src/main/java/com/shop/ecommerce/dto/CartItemResponse.java
+++ src/main/java/com/shop/ecommerce/dto/CartItemResponse.java
@@ -1,5 +1,10 @@
 package com.shop.ecommerce.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class CartItemResponse {
     private String productName;
     private Integer quantity;
