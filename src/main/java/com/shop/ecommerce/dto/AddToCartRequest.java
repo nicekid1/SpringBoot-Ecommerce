2c03b95aaf03ddfde5bef430d0da95f05db1ead2
alimohtarami <alimohtarami5@gmail.com>
2025-07-31 18:58:41 +0330

    Cart dto

--- src/main/java/com/shop/ecommerce/dto/AddToCartRequest.java
+++ src/main/java/com/shop/ecommerce/dto/AddToCartRequest.java
@@ -1,4 +1,9 @@
 package com.shop.ecommerce.dto;
 
+import lombok.Data;
+
+@Data
 public class AddToCartRequest {
+    private Long productId;
+    private Integer quantity;
 }
--- src/main/java/com/shop/ecommerce/dto/CartItemResponse.java
+++ src/main/java/com/shop/ecommerce/dto/CartItemResponse.java
@@ -1,5 +1,7 @@
 package com.shop.ecommerce.dto;
 
 public class CartItemResponse {
-
+    private String productName;
+    private Integer quantity;
+    private Double price;
 }
